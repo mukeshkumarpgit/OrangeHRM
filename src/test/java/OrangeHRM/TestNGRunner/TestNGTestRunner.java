8e943f8e5dbcfb86feccfbdace38ddd56d6ecd17
mukeshkumarpgit <mukeshpsgauto@gmail.com>
2024-11-28 21:04:03 +0530

    SecondCommit

--- src/test/java/OrangeHRM/TestNGRunner/TestNGTestRunner.java
+++ src/test/java/OrangeHRM/TestNGRunner/TestNGTestRunner.java
@@ -1,5 +1,10 @@
 package OrangeHRM.TestNGRunner;
 
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features="src/test/java", glue="OrangeHRM/stepDefinitions", 
+monochrome=true, plugin= {"html:target/cucumber.html"})
+
 public class TestNGTestRunner {
 
 }
